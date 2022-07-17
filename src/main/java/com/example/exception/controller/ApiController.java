963bea08e56e89b7183af7f2e0e8da9c31d36715
columbooster <kinglove0403@gmail.com>
2022-07-17 21:39:36 +0900

    exception

--- src/main/java/com/example/exception/controller/ApiController.java
+++ src/main/java/com/example/exception/controller/ApiController.java
@@ -1,14 +1,19 @@
 package com.example.exception.controller;
 
 import com.example.exception.dto.User;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/user")
 public class ApiController {
 
     @GetMapping("")
-    public User get(@RequestParam(required = false) String name, Integer age) {
+    public User get(@RequestParam(required = false) String name,@RequestParam(required = false) Integer age) {
         User user = new User();
         user.setName(name);
         user.setAge(age);
@@ -19,11 +24,19 @@ public class ApiController {
     }
 
     @PostMapping("")
-    public User post(@RequestBody User user) {
+    public User post(@Valid @RequestBody User user) {
 
         System.out.println(user);
         return user;
     }
+
+
+    @ExceptionHandler(value = MethodArgumentNotValidException.class)
+    public ResponseEntity methodArgumentNotValidException(MethodArgumentNotValidException e){
+        System.out.println("api controller");
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+    }
+
 
     // exception 사례 알아보기
     // Spring legacy  와 spring boot 비교
